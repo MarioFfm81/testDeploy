04e0e307984fdb9d5365f41a3e9895f03ec01f08
Mario Niemann <mario.niemann@t-online.de>
2018-11-22 20:54:49 +0100

    testDeploy

---
+++ main.py
@@ -0,0 +1,10 @@
+import flask
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>TestDeploy</h1>"
+
+if __name__ == '__main__':
+    app.run()
